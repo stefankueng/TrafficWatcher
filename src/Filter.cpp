d5d2b2295032a91cad9c3f54cba1cdfeec38f1a2
Stefan Küng <665591+stefankueng@users.noreply.github.com>
2008-05-03 18:44:48 +0000

    Improve the filter.

--- src/Filter.cpp
+++ src/Filter.cpp
@@ -3,6 +3,10 @@
 
 CFilter::CFilter(void)
 {
+	for (int i=0; i<FILTER_MEAN_DEPTH; ++i)
+		diffs[i] = 0;
+	dwTotalOld = 0;
+	dwPrevTicks = 0;
 }
 
 CFilter::~CFilter(void)
@@ -17,5 +21,14 @@ DWORD64 CFilter::Filter(DWORD ticks, DWORD64 value)
 	DWORD64 diffValue = value - dwTotalOld;
 	dwTotalOld = value;
 
-	return diffValue *1000 / diffTicks;
+	DWORD64 meanDiff = diffValue;
+	for (int i=0; i<FILTER_MEAN_DEPTH; ++i)
+		meanDiff += diffs[i];
+	meanDiff /= (FILTER_MEAN_DEPTH+1);
+
+	for (int i=FILTER_MEAN_DEPTH-1; i>0; --i)
+		diffs[i] = diffs[i-1];
+	diffs[0] = diffValue;
+
+	return meanDiff * 1000 / diffTicks;
 }
