dd57d73a966f95e03d657293b4f136642af9cdeb
steveking <steveking@4d2af418-de0a-4c33-94f8-8d8a36d230ed>
2008-05-04 13:46:25 +0000

    Add GPL headers.

--- trunk/src/HyperLink.h
+++ trunk/src/HyperLink.h
@@ -1,6 +1,6 @@
-// TortoiseSVN - a Windows shell extension for easy version control
+// TrafficWatcher - a network speed monitor
 
-// Copyright (C) 2003-2007 - TortoiseSVN
+// Copyright (C) 2008 - Stefan Kueng
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
