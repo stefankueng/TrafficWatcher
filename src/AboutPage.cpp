dce90f368a9ef41c24aed385e15b9a02a85ef665
Stefan Küng <665591+stefankueng@users.noreply.github.com>
2008-05-04 13:42:00 +0000

    * add build script * add version information to the about dialog * create the version resource from the build script to include the svn revision

--- src/AboutPage.cpp
+++ src/AboutPage.cpp
@@ -4,13 +4,16 @@
 #include "stdafx.h"
 #include "trafficwatch.h"
 #include "AboutPage.h"
+#include "version.h"
+// compiler error because of missing version.h file?
+// run the NAnt build script to generate the version.h file from the version.in
+// file. Or at least copy the file version.in to version.h
 
-
-// CAboutPage dialog
 
 IMPLEMENT_DYNAMIC(CAboutPage, CPropertyPage)
 CAboutPage::CAboutPage()
 	: CPropertyPage(CAboutPage::IDD)
+	, m_VersionString(_T(""))
 {
 }
 
@@ -23,6 +26,7 @@ void CAboutPage::DoDataExchange(CDataExchange* pDX)
 	CPropertyPage::DoDataExchange(pDX);
 	DDX_Control(pDX, IDC_SOURCECODELINK, m_sourceLink);
 	DDX_Control(pDX, IDC_WEBLINK, m_webLink);
+	DDX_Text(pDX, IDC_VERSIONINFO, m_VersionString);
 }
 
 
@@ -35,8 +39,11 @@ BOOL CAboutPage::OnInitDialog()
 {
 	CPropertyPage::OnInitDialog();
 
+	m_VersionString.Format(_T("Version %d.%d.%d.%d"), VER_MAJOR, VER_MINOR, VER_MICRO, VER_REVISION);
 	m_webLink.SetURL(_T("http://tools.tortoisesvn.net"));
 	m_sourceLink.SetURL(_T("http://code.google.com/p/trafficwatcher/source/browse"));
+
+	UpdateData(FALSE);
 
 	return TRUE;  
 }
