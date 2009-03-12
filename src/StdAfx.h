784abd956a4b477561f51f5503f73bc5e4f52197
Stefan Küng <665591+stefankueng@users.noreply.github.com>
2009-03-12 19:31:01 +0000

    Patch from fastcat: Fixed issue #5: Tray tooltip truncated at 64 chars

--- src/StdAfx.h
+++ src/StdAfx.h
@@ -1,15 +1,15 @@
 #pragma once
 
 #ifndef WINVER
-#	define WINVER 0x0600
+#	define WINVER 0x0501
 #endif
 
 #ifndef _WIN32_WINNT
-#	define _WIN32_WINNT 0x0600
+#	define _WIN32_WINNT 0x0501
 #endif						
 
 #ifndef _WIN32_WINDOWS
-#	define _WIN32_WINDOWS 0x0600
+#	define _WIN32_WINDOWS 0x0501
 #endif
 
 #ifndef _WIN32_IE
